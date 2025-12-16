e7980641b2351fe0d95f6b7608fbf71a185ffca8
Sanawllah <sanawllah@gmail.com>
2025-12-16 18:15:10 +0300

    Prepare for production

--- ServisOfficeClone/settings.py
+++ ServisOfficeClone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kmzetdmi$v$#0+y1ad#-ebs9=@ty0&jrxhnw+iu2*%$26nzuks'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
